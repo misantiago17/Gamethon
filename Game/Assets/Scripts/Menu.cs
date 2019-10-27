fa5e9904ff3003b74ab7a07a85e258eb8443497a
lucas3311 <lucaslimareis3311@gmail.com>
2019-10-27 08:17:41 -0300

    a

--- Game/Assets/Scripts/Menu.cs
+++ Game/Assets/Scripts/Menu.cs
@@ -16,6 +16,11 @@ public class Menu : MonoBehaviour
         SceneManager.LoadScene(2);
     }
 
+    public void toMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
     // Update is called once per frame
     void Update()
     {
